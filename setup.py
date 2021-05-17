c2214db12b7a0678e83337c488a039c091b4fc56
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2021-05-17 16:56:42 +0200

    Added a setup file for the package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='nasbench_pytorch',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'torchvision'
+    ]
+)
