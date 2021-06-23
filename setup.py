1a7a556069cd4bc0a0e5faad9c37e28ffd6c76f9
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2021-06-23 18:53:12 +0200

    Don't have torchvision as a requirement.

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@ import setuptools
 setuptools.setup(
     name='nasbench_pytorch',
     version='0.1',
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'torchvision'
-    ]
+    packages=setuptools.find_packages()
 )
