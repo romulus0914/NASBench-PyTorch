37bc332bc6802e0375d8cca839d54f47524487fb
Gabriela Kadlecova <gabi.suchoparova@gmail.com>
2023-05-12 14:03:06 +0200

    New minor version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='nasbench_pytorch',
-    version='1.3',
+    version='1.3.1',
     license='Apache License 2.0',
     author='Romulus Hong, Gabriela Kadlecová',
     packages=setuptools.find_packages()
